6df2b65067fda53fa2a94db383a67fe22195ffc1
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-24 23:04:13 +0300

    router working now

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const articleRouter = require("./routes/articles");
 const app = express();
 
 app.set("view engine", "ejs");
+
+app.use("/articles", articleRouter);
 
 // create route
 app.get("/", (req, res) => {
